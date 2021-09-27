e1eb859481760dd7859e8e2cfd42aa8392a7ee1c
kksumm123 <kksumm123@gmail.com>
2021-09-27 17:44:29 +0900

    1. 이미지 스프라이트로 4:3 , 12개로 자름 2. Main씬에 canvas추가, 해상도 비율 맞춤 3. PuzzlePiecePoint.cs 추가 및 작성 완료 4. 리스트에 스프라이트 12개 등록 5. Images RectTransform X, Y포지션 조정

---
+++ Assets/PuzzlePiecePoint.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PuzzlePiecePoint : MonoBehaviour
+{
+    public List<Sprite> sprites;
+    public int xCount = 4;
+    public int yCount = 3;
+
+    private void Awake()
+    {
+        InitPosition();
+    }
+
+    [ContextMenu("퍼즐 배치")]
+    private void InitPosition()
+    {
+        DeleteOldPiece();
+        float pieceWidth = sprites[0].textureRect.width;
+        float pieceHeight = sprites[0].textureRect.height;
+
+        int imageIndex = 0;
+        for (int y = 0; y < yCount; y++)
+        {
+            for (int x = 0; x < xCount; x++)
+            {
+                var item = new GameObject($"{x} : {y}");
+                item.transform.parent = transform;
+                RectTransform rt = item.AddComponent<RectTransform>();
+                //크기
+                rt.sizeDelta = new Vector2(pieceWidth, pieceHeight);
+
+                //위치
+                float xPos = x * pieceWidth;
+                float yPos = y * pieceHeight;
+                item.transform.localPosition = new Vector3(xPos, yPos);
+
+                item.AddComponent<Image>().sprite = sprites[imageIndex];
+                imageIndex++;
+            }
+        }
+    }
+
+    private void DeleteOldPiece()
+    {
+        var childs = transform.GetComponentsInChildren<Image>();
+        foreach (var item in childs)
+        {
+            DestroyImmediate(item.gameObject);
+        }
+    }
+}
