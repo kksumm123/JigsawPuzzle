d96e28100ec20572e6e23b4c3490474a31106e60
kksumm123 <kksumm123@gmail.com>
2021-09-27 21:38:45 +0900

    히히힛

--- Assets/PuzzlePiecePoint.cs
+++ Assets/PuzzlePiecePoint.cs
@@ -7,20 +7,49 @@ using Random = UnityEngine.Random;
 
 public class PuzzlePiecePoint : MonoBehaviour
 {
-    public Transform Pieces;
+    public Texture2D originalTexture;
+    public Transform pieceParent;
     public List<Sprite> sprites;
     public int xCount = 4;
     public int yCount = 3;
 
     private void Awake()
     {
+        SliceImage();
         InitPosition();
+    }
+
+    [ContextMenu("!!!이미지 짤라뻐려!!!")]
+    void SliceImage()
+    {
+        float width = originalTexture.width / xCount;
+        float height = originalTexture.height / yCount;
+        Vector2 pivot = new Vector2(0.5f, 0.5f);
+        float ppu = 100;
+
+        sprites.Clear();
+        for (int y = 0; y < yCount; y++)
+        {
+            for (int x = 0; x < xCount; x++)
+            {
+                var newSprite = Sprite.Create(originalTexture,
+                                                new Rect(x * width,
+                                                         originalTexture.height - ((y + 1) * height),
+                                                         width,
+                                                         height),
+                                                pivot,
+                                                ppu);
+                newSprite.name = $"{x} : {y}";
+                sprites.Add(newSprite);
+            }
+        }
     }
 
     [ContextMenu("퍼즐 배치")]
     private void InitPosition()
     {
-        DeleteOldPiece();
+        DeleteOldPiece(transform);
+        DeleteOldPiece(pieceParent);
         float pieceWidth = sprites[0].textureRect.width;
         float pieceHeight = sprites[0].textureRect.height;
 
@@ -42,7 +71,7 @@ public class PuzzlePiecePoint : MonoBehaviour
                 item.transform.localPosition = new Vector3(xPos, yPos);
 
                 item.AddComponent<Image>().sprite = sprites[imageIndex];
-                images.Add(Instantiate(item, Pieces));
+                images.Add(Instantiate(item, pieceParent));
 
                 var curImage = item.GetComponent<Image>();
                 var curImageColor = curImage.color;
@@ -53,20 +82,32 @@ public class PuzzlePiecePoint : MonoBehaviour
         }
         foreach (var item in images)
         {
-            Destroy(item.GetComponent<FixedPiece>());
+            if (item == null)
+                continue;
+
+            if (Application.isPlaying)
+                Destroy(item.GetComponent<FixedPiece>());
+            else
+                DestroyImmediate(item.GetComponent<FixedPiece>());
+
             item.AddComponent<Piece>();
-            item.transform.position = 
+            item.transform.position =
                 new Vector3(Random.Range(0 + pieceWidth * 0.5f, Camera.main.pixelWidth),
                             Random.Range(0 + pieceHeight * 0.5f, Camera.main.pixelHeight));
         }
+
+        GameManager.Instance.completeScore = sprites.Count * 100;
     }
 
-    private void DeleteOldPiece()
+    private void DeleteOldPiece(Transform tr)
     {
-        var childs = transform.GetComponentsInChildren<Image>();
+        var childs = tr.GetComponentsInChildren<Image>();
         foreach (var item in childs)
         {
-            DestroyImmediate(item.gameObject);
+            if (Application.isPlaying)
+                Destroy(item.gameObject);
+            else
+                DestroyImmediate(item.gameObject);
         }
     }
 }
