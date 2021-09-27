af679df63ffb270819998960e4786bee6bdf5e48
kksumm123 <kksumm123@gmail.com>
2021-09-27 20:13:56 +0900

    위치에 정확히맞으면 1. 반짝이고 2. 점수증가 3. Piece 컴포넌트 꺼버리기

--- Assets/FixedPiece.cs
+++ Assets/FixedPiece.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -13,7 +14,37 @@ public class FixedPiece : MonoBehaviour, IDropHandler
         string log = $"{eventData.pointerDrag.name} 오브젝트가 {name}에 드랍됨";
         Debug.Log(log, transform);
 
-        if (eventData.pointerDrag.name == name)
+        // 올바른 위치 드랍되면
+        if (IsRighPosition(eventData.pointerDrag))
+        {
+            var draggedImage = eventData.pointerDrag.GetComponent<Image>();
+            // 반짝이게
+            StartCoroutine(BlickCo(draggedImage));
+
+            // 점수 증가
+            //GameManager.Instance.AddScore(100);
+
+            // Piece 컴포넌트 끄기
             eventData.pointerDrag.GetComponent<Piece>().enabled = false;
+        }
+    }
+
+    bool IsRighPosition(GameObject pointerDrag)
+    {
+        if (pointerDrag.name == name)
+            return true;
+
+        return false;
+    }
+
+    float blinkTime = 0.1f;
+    IEnumerator BlickCo(Image image)
+    {
+        image.color = Color.red;
+        yield return new WaitForSeconds(blinkTime);
+        image.color = Color.white;
+
+        // 드래그된 것 못 움직이게
+        image.raycastTarget = false;
     }
 }
